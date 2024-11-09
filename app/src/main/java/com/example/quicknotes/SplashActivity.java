ec040f8b7ae70d11af254746accfe9f15f2aa743
Vinoth0503 <master5vinoth@gmail.com>
2024-11-09 10:23:01 +0530

    2st Commit

---
+++ app/src/main/java/com/example/quicknotes/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.quicknotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Delay for 2 seconds (2000 milliseconds)
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start HomeActivity
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish(); // Close SplashActivity
+            }
+        }, 2000); // 2000 milliseconds = 2 seconds
+    }
+}
